7f4fd774e3430be1c4b7abce5dc935092f17716c
Andrew Trenkal <trenkal.andrey@mail.ru>
2019-09-13 15:16:26 +0900

    added ability to select line thickness

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {fromEvent} from 'rxjs';
-import {map, pairwise, switchMap, takeUntil} from 'rxjs/operators'
+import {map, pairwise, switchMap, takeUntil, startWith, withLatestFrom} from 'rxjs/operators'
 
 const canvas = document.querySelector('canvas');
 const rect = canvas.getBoundingClientRect();
@@ -9,21 +9,33 @@ const scale = window.devicePixelRatio;
 canvas.width = rect.width * scale;
 canvas.height = rect.height * scale;
 
+const range = document.getElementById('range');
+const color = document.getElementById('color');
+
 const mouseMove$ = fromEvent(canvas, 'mousemove');
 const mouseDown$ = fromEvent(canvas, 'mousedown');
 const mouseUp$ = fromEvent(canvas, 'mouseup');
+const mouseOut$ = fromEvent(canvas, 'mouseout');
+const range$ = fromEvent(range, 'input')
+    .pipe(
+        map(e => e.target.value),
+        startWith(range.value)
+    );
 
 const stream$ = mouseDown$
     .pipe(
-        switchMap(() => {
+        withLatestFrom(range$, (_, range) => range),
+        switchMap((lineWidth) => {
             return mouseMove$
                 .pipe(
                     map(e => ({
                         x: e.offsetX,
-                        y: e.offsetY
+                        y: e.offsetY,
+                        lineWidth
                     })),
                     pairwise(),
-                    takeUntil(mouseUp$)
+                    takeUntil(mouseUp$),
+                    takeUntil(mouseOut$)
                 )
             }
         )
@@ -31,6 +43,9 @@ const stream$ = mouseDown$
 
 stream$
     .subscribe(([from, to]) => {
+        const {lineWidth} = from;
+
+        ctx.lineWidth = lineWidth;
         ctx.beginPath();
         ctx.moveTo(from.x, from.y);
         ctx.lineTo(to.x, to.y);
